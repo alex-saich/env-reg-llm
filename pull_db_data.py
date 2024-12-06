a138f532ff4a5b1773e2d2318e6a3ccf9629582d
alex-saich <alexsaich@gmail.com>
2024-12-05 23:06:00 -0500

    added psycopg2-binary to reqs

--- pull_db_data.py
+++ pull_db_data.py
@@ -1,7 +1,7 @@
-from store_pdfs_pg import get_db_connect
+from store_pdfs_pg import get_db_connection
 
 def pull_project_names():
-    conn = get_db_connect()
+    conn = get_db_connection()
     cur = conn.cursor()
     cur.execute("SELECT project_name FROM projects")
     project_names = [row[0] for row in cur.fetchall()]
